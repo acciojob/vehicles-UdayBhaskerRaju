90c6aab51a381e6b5e4064b17878d29286944624
Uday Bhasker Raju <udaybhaskerraju@example.com>
2024-04-16 01:28:34 -0700

    assignment 2 second commit with some changes

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,17 +9,31 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        super(name);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
         //Hint: Car extends Vehicle
     }
-
     public void changeGear(int newGear){
-
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
-
     public void changeSpeed(int newSpeed, int newDirection){
-
+        setCurrentSpeed(newSpeed);
+        setCurrentDirection(newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,11 +3,12 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super(name, 4,4,5,isManual,"seden",5);
         //Use arbitrary values for parameters which are not mentioned
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = getCurrentSpeed() + rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -18,13 +19,36 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
+        //for all other cases, change the gear accordingly
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            setGears(0);
         }
-        //for all other cases, change the gear accordingly
-
-        if(newSpeed > 0) {
+        else if(newSpeed > 0 && newSpeed <=50) {
             changeSpeed(newSpeed, getCurrentDirection());
+            setGears(1);
+        }
+        else if(newSpeed > 50 && newSpeed <=100) {
+            changeSpeed(newSpeed, getCurrentDirection());
+            setGears(2);
+        }
+        else if(newSpeed > 100 && newSpeed <=150) {
+            changeSpeed(newSpeed, getCurrentDirection());
+            setGears(3);
+        }
+        else if(newSpeed > 150 && newSpeed <=200) {
+            changeSpeed(newSpeed, getCurrentDirection());
+            setGears(4);
+        }
+        else if(newSpeed > 200 && newSpeed <=250) {
+            changeSpeed(newSpeed, getCurrentDirection());
+            setGears(5);
+        }
+        else {
+            changeSpeed(newSpeed, getCurrentDirection());
+            setGears(6);
         }
     }
+
 }
